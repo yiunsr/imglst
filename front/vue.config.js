1675fd051096858e051cce9862749d9b5b9f9a3f
yiunsr <yiunsr@hanmail.net>
2022-10-03 22:48:33 +0900

    환경설정 변경

--- front/vue.config.js
+++ front/vue.config.js
@@ -7,6 +7,13 @@ module.exports = defineConfig({
     'vuetify'
   ],
   devServer: {
+    // output 파일에 대해 hash 를 붙인다.
+    // configureWebpack: config => {
+    //   if (process.env.NODE_ENV === 'production') {
+    //     config.output.filename = 'js/[name].[hash].js'
+    //     config.output.chunkFilename = 'js/[name].[hash].js'
+    //   }
+    // },
     proxy: {
       '^/demo/': {
         target: 'http://localhost:8000',
